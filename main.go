c2fbf3c535a96c1cf5e44a57f3e4d920e733f44c
toewailin <toewailinbdl@gmail.com>
2023-03-13 23:48:49 +0630

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	_ "github.com/sm-go/my-api/routers"
+
+	"github.com/beego/beego/v2/client/orm"
+	beego "github.com/beego/beego/v2/server/web"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	sqlConn, err := beego.AppConfig.String("sqlconn")
+	if err != nil {
+		log.Fatal(err)
+	}
+	orm.RegisterDataBase("default", "mysql", sqlConn)
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
---
+++ routers/router.go
@@ -0,0 +1,38 @@
+// @APIVersion 1.0.0
+// @Title beego Test API
+// @Description beego has a very cool tools to autogenerate documents for your API
+// @Contact astaxie@gmail.com
+// @TermsOfServiceUrl http://beego.me/
+// @License Apache 2.0
+// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
+package routers
+
+import (
+	"github.com/sm-go/my-api/controllers"
+
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+func init() {
+	ns := beego.NewNamespace("/v1",
+
+		beego.NSNamespace("/image",
+			beego.NSInclude(
+				&controllers.ImageController{},
+			),
+		),
+
+		beego.NSNamespace("/posts",
+			beego.NSInclude(
+				&controllers.PostsController{},
+			),
+		),
+
+		beego.NSNamespace("/users",
+			beego.NSInclude(
+				&controllers.UsersController{},
+			),
+		),
+	)
+	beego.AddNamespace(ns)
+}
